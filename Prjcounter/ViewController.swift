a9651397c9970b795736478ea45aa9bb31b191d5
csd5766 <csd5766@naver.com>
2018-03-20 15:39:38 +0900

    Reset

--- Prjcounter/ViewController.swift
+++ Prjcounter/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var lbl1: UILabel!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func btn1(_ sender: Any) {
+    counter = counter + 1
+        lbl1.text = ("\(counter)")
+    }
+    @IBAction func btn2(_ sender: Any) {
+        counter = 0
+        
+        lbl1.text = ("\(counter)")
+        
+    }
 
 }
 
